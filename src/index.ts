14b0403d75dd29a78491ffae4fb34d1aa6b22f32
Jordy Fontoura <jordyfontoura@outlook.com>
2024-07-18 18:35:34 -0300

    feat: Update exports in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,16 @@
+import * as result from './result';
+import * as future from './future';
+import * as utilities from './utilities';
+import * as extensios from './extensios';
+
 export * from './result';
 export * from './future';
 export * from './utilities';
 export * from './extensios';
+
+export default {
+  ...result,
+  ...future,
+  ...utilities,
+  ...extensios
+}
