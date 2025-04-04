86e2a019089edb5cee3c2486fb316bd0e0b379d8
Jordy Fontoura <jordyfontoura@outlook.com>
2025-04-04 13:38:07 -0300

    feat: add Rollup configuration and TypeScript build setup

---
+++ rollup.config.js
@@ -0,0 +1,35 @@
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import terser from '@rollup/plugin-terser';
+import dts from 'rollup-plugin-dts';
+
+export default [
+  {
+    input: './dist/index.js',
+    output: [
+      {
+        file: './dist/index.cjs.js',
+        format: 'cjs',
+        sourcemap: true
+      },
+      {
+        file: './dist/index.esm.js',
+        format: 'esm',
+        sourcemap: true
+      }
+    ],
+    plugins: [
+      resolve(),
+      commonjs(),
+      terser()
+    ]
+  },
+  {
+    input: './dist/index.d.ts',
+    output: {
+      file: './dist/index.d.ts',
+      format: 'es'
+    },
+    plugins: [dts()]
+  }
+];
